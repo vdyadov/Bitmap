50ea6a4bf6ceef969c361dd71180a6ec49e67721
vdyadov <vdyadov@yandex.ru>
2021-06-20 21:54:09 +0700

    Добавил функцию по установке конкретного бита

--- README.md
+++ README.md
@@ -18,9 +18,16 @@
 * ```unsigned short const bit_idx``` - индекс требуемого бита
 * ```unsigned char *p_bit``` - указатель на переменную, в которую будет записано значение бита.
 ***
+```int bitmap__set_bit(struct Bitmap const *p_bitmap, unsigned short const bit_idx)```
+ 
+ Установка нужного бита. В функцию требуется передать валдиный битмап и номер бита, который необходимо установить.
+
+* ```struct Bitmap const *p_bitmap``` - указатель на битовый массив.
+* ```unsigned short const bit_idx``` - индекс требуемого бита.
+***
 ```int bitmap__get_size(struct Bitmap const *p_bitmap, unsigned short *p_size)```
 
 Получение размера массива в битах. Битмап должен быть валидным битмапом.
 
 * ```struct Bitmap const *p_bitmap``` - указатель на битовый массив.
-* ```unsigned char *p_bit``` - указатель на ппеременную, в которую будет записан размер
+* ```unsigned char *p_bit``` - указатель на переменную, в которую будет записан размер
---
+++ test/unit/bitmap_set_test.c
@@ -0,0 +1,46 @@
+#include "../../include/bitmap.h"
+
+#include <tap.h>
+
+#include <stdio.h>
+#include <malloc.h>
+
+//A simple test with libtap
+//compile with -l tap
+int main(void)
+{
+    plan(7);
+    struct Bitmap bmap = {NULL, 0, 0};
+    unsigned char bit = 0;
+    
+    ok(-1 == bitmap__set_bit(NULL, 0), "acessing a NULL fails with -1");
+
+    ok(-2 == bitmap__set_bit(&bmap, 0), 
+                             "acessing an uncreated bitmap fails with -2");
+
+    bmap.magic = BITMAP_MAGIC;
+    ok(-3 == bitmap__set_bit(&bmap, 0), "acessing bitmap with bit_array == NULL"
+                                        " fails with -3");
+
+    bmap.bit_array = malloc(sizeof(unsigned long));
+    bmap.size = sizeof(unsigned long) * 8;
+    
+    ok(-4 == bitmap__set_bit(&bmap, bmap.size + 1), 
+                             "acessing a bitmap with allocated bit_array"
+                             " but with size == 0 fails with -4");
+
+    //0101000... is how I want the number to look
+    
+    bitmap__set_bit(&bmap, 1);
+    bitmap__set_bit(&bmap, 3);
+
+    bitmap__get_bit(&bmap, 1, &bit);
+    ok(bit, "bit 1 installed correctly");
+    bitmap__get_bit(&bmap, 2, &bit);
+    ok(0 == bit, "bit 2 extracted correctly");
+    bitmap__get_bit(&bmap, 3, &bit);
+    ok(bit, "bit 3 installed correctly");
+
+    free(bmap.bit_array);
+    return 0;
+}
