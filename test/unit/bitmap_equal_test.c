b1adf9f7cb37dfdcaf2a29903bf50035ad55b5c4
vdyadov <vdyadov@yandex.ru>
2021-06-24 12:33:10 +0700

    Добавил функцию сравнения двух массивов

---
+++ test/unit/bitmap_equal_test.c
@@ -0,0 +1,58 @@
+#include "../../include/bitmap.h"
+
+#include <tap.h>
+
+#include <stdio.h>
+#include <malloc.h>
+
+//A simple test with libtap
+//compile with -l tap
+int main(void)
+{
+    struct Bitmap bmap = {NULL, 0, 0};
+    struct Bitmap bmap_2 = {NULL, 0, 0};
+
+    plan(7);
+    
+    ok(-1 == bitmap__and(NULL, NULL), "acessing a NULL fails with -1");
+
+    ok(-2 == bitmap__and(&bmap, &bmap_2), 
+                             "magic number uncorrect fails with -2");
+    
+    bmap.magic = BITMAP_MAGIC;
+    ok(-3 == bitmap__and(&bmap, &bmap_2), "acessing bitmap with bit_array == NULL"
+                                        " fails with -3");
+
+    bmap.bit_array = calloc(1, sizeof(unsigned long));
+    bmap.size = sizeof(unsigned long) * 8;
+    
+    ok(-4 == bitmap__and(&bmap, NULL), "acessing a NULL fails with -4");
+
+    ok(-5 == bitmap__and(&bmap, &bmap_2), 
+                             "magic number uncorrect fails with -2");
+
+    bmap_2.magic = BITMAP_MAGIC;
+    ok(-6 == bitmap__and(&bmap, &bmap_2), "acessing bitmap with bit_array == NULL"
+                                        " fails with -6");
+    
+    bmap_2.bit_array = calloc(1, sizeof(unsigned long));
+    bmap_2.size = sizeof(unsigned long) * 8;
+
+    // 01011001... is how I want the number to look
+    bitmap__set_bit(&bmap, 1);
+    bitmap__set_bit(&bmap, 3);
+    bitmap__set_bit(&bmap, 4);
+    bitmap__set_bit(&bmap, 7);
+
+    // 01011001... is how I want the number to look
+    bitmap__set_bit(&bmap_2, 1);
+    bitmap__set_bit(&bmap_2, 3);
+    bitmap__set_bit(&bmap_2, 4);
+    bitmap__set_bit(&bmap_2, 7);
+
+    ok(0 == bitmap__equal(&bmap, &bmap_2), "these bitmaps is equal");
+
+    free(bmap_2.bit_array);
+    free(bmap.bit_array);
+    return 0;
+}
